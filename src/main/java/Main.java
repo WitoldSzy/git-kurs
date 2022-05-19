fa69babef01ce6784b7567867185ac620f630ec3
Witold Szymański <witold.szymanski.it@gmail.com>
2022-05-19 13:29:14 +0200

    Zmiana w drugim repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
     }
+
+    private void displayText()
+    System.out.println("Zmiana w drugim repo");
 }
